c2fd2e26acd7c559579ebc18c3ed3a3a6b25d2e8
Torro <torro@tuta.io>
2020-12-03 13:27:07 +0100

    add comment about formatting

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ channels = [
     ("https://euc-live.fl.freecaster.net/live/eucom/ebsp-audio_eng=96000-video=1600000.m3u8", xbmcgui.ListItem("EbS+"))
 ]
 # fmt: on
+# If you dislike my formatting and would like the black formatter to handle it please open an issue and let me know.
+
 
 xbmcplugin.addDirectoryItems(handle, channels)
 xbmcplugin.endOfDirectory(handle)
